95c014fcc00873764ab377199799fb4e7c1ce834
Ivan Zhyhalkin <ivan.zhigalkin@gmail.com>
2022-08-03 18:06:34 +0300

    Add project files.

---
+++ Aps-task7-models/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using Aps_task7_models.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Aps_task7_models.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(Data data)
+        {
+            Debug.Print("Data inside class " + data.First + " " + data.Second + " " + data.Count);
+            return View();
+        }
+    }
+}
---
+++ Aps-task7-models/Models/Data.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Aps_task7_models.Models
+{
+    public class Data
+    {
+        public string First { get; set; }
+        public string Second { get; set; }
+        public int Count { get; set; }
+    }
+}
